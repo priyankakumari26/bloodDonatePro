876e99d319cd87d99e9a27db61f327471c8d6c9c
chetan <chetan@PTMM69.local>
2018-09-28 17:59:32 +0530

    required blood functionality

--- src/app/blood-data/blood-data.component.ts
+++ src/app/blood-data/blood-data.component.ts
@@ -12,8 +12,6 @@ interface IEachBloodCount {
   styleUrls: ['./blood-data.component.scss']
 })
 export class BloodDataComponent implements OnInit {
-
-  constructor() { }
   private currentSockInBank: IBloodRepo = {
     aPos: {
       currentStock: 0,
@@ -53,13 +51,17 @@ export class BloodDataComponent implements OnInit {
     oNeg: {
       currentStock: 0,
       canReceiveFrom: receiversList.oNegCanReceiveFrom,
-      displayName: '0 -'
+      displayName: 'o-'
     }
   }
   private allGroups: string[] = Object.keys(this.currentSockInBank);
   private shouldUpdateStockorCalculateUnit: boolean = true;
   private stockUpdateGroup: string = '';
   private stockUpdateCount: string = '0';
+  private requredBloodGroup: string = '';
+  private requiredBlood: string = '0';
+  private bloodDonar = {};
+  constructor() { }
   ngOnInit() {
   }
   private updateStockorCalculate(isCalcualtionReq: boolean): void {
@@ -80,6 +82,45 @@ export class BloodDataComponent implements OnInit {
   private showUpdateStockForm(): void {
     this.shouldUpdateStockorCalculateUnit = true;
   }
+  checkAvailability(bloodGroup): void{
+     console.log(bloodGroup);
+     let requiredGroups;
+     for(let key in this.currentSockInBank){
+         if(key === bloodGroup){
+          requiredGroups = this.currentSockInBank[bloodGroup]["canReceiveFrom"];
+          break;
+         }
+     }
+     for(let key in this.currentSockInBank){
+       for(let i = 0; i < requiredGroups.length; i++){
+         if(key === requiredGroups[i] && this.currentSockInBank[key]["currentStock"] >= this.requiredBlood){
+          this.bloodDonar[key] = this.currentSockInBank[key];
+         }
+       }
+    }
+    console.log(this.bloodDonar);
+  }
+
+  private allowDrop(event: Event): void {
+    event.preventDefault(); 
+}
+
+onDrop(dropData: any): void {
+  //dropData.preventDefault();
+  //this.dropOverActive = false;
+  let droppedData = dropData;
+  //debugger;
+  let sourceFile = JSON.parse(dropData.dataTransfer.getData("file"));
+  
+}
+
+private allowdrag(event: DragEvent, grpname: any): void {
+  event.dataTransfer.setData("donar",JSON.stringify(grpname));
+  //debugger;
+   //event.preventDefault();
+}
+
+
 
 
 }
